ff352a16e58ee22c2f8cc02e40e49e71e938b105
Pongsakorn <pcherng01@gmail.com>
2018-02-01 01:25:55 -0800

    Initial Commit

---
+++ Kontac-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kontac-Swift
+//
+//  Created by Pongsakorn on 2/1/18.
+//  Copyright © 2018 Pongsakorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
